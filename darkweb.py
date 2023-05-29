31c6f5605d155336902d3130e4a695b8d43655ed
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-05-29 21:08:48 +0100

    Add files via upload

---
+++ darkweb.py
@@ -0,0 +1,178 @@
+from time import sleep
+import random
+# ideas: dark-mart
+
+
+# variables
+
+# not needed:
+# user = "John Kennedy"
+# password = "fishman72"
+
+commands_ran = 0
+
+# Beginning 
+print("\n\n\n")
+print("Daniel Catarig presents...")
+sleep(3)
+print("""
+
+
+▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀    █     █░▓█████  ▄▄▄▄   
+▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒    ▓█░ █ ░█░▓█   ▀ ▓█████▄ 
+░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░    ▒█░ █ ░█ ▒███   ▒██▒ ▄██
+░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄    ░█░ █ ░█ ▒▓█  ▄ ▒██░█▀  
+░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄   ░░██▒██▓ ░▒████▒░▓█  ▀█▓
+ ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒   ░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒
+ ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░     ▒ ░ ░   ░ ░  ░▒░▒   ░ 
+ ░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░      ░   ░     ░    ░    ░ 
+   ░          ░  ░   ░     ░  ░          ░       ░  ░ ░      
+ ░                                                         ░ 
+                        A DAN INC.
+                           GAME
+
+""")
+sleep(2)
+print("""
+
+
+
+
+
+
+
+
+""")
+sleep(1)
+print("""
+
+
+
+
+
+
+
+
+""")
+sleep(1)
+print("""
+
+
+
+
+
+
+
+
+""")
+sleep(1)
+print("""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""")
+sleep(1)
+print("You boot your computer up with a rush of adrenaline. You've decided to explore the decrepit depths of the Dark Web. \n Soon your monitor screen flickers on, radiating a milky white light into your otherwise pitch black room.\n\n")
+sleep(8)
+print("The Login Page stands before you.\n\n\n")
+sleep(2)
+
+print("8------------------------------------------------------8")
+user = input("Please enter in your username: ")
+password = input("That is correct. Please enter your password: ")
+
+sleep(3)
+print("\n\nSuccessfully logged in.\n\n\n\n\n\n\n\n\n")
+sleep(2)
+continue1 = input("""
+
+You are smacked by another overpowering boost of adrenaline as you slowly edge your mouse cursor over to DarK_XZY, a special program 
+which lets you plunge into the mysterious void that is the Dark Web.
+Click! Click! You open the program.
+A terminal appears. Shakily you move your cursor over to a command line. You enter in the command:
+
+EXP10RE --DARK_WB
+
+
+Your screen suddenly lights up. The fans in your pc start to whirr vigorously. Perhaps this wasn't such a good idea, but it's too late now...
+The speakers start letting out rapid beeping noises, as if it was crying out from the immense strain on the computer using morse code.
+Everything begins to pixelate, and odd colours start flashing randomly across the screen. And then... nothing. Your computer's CPU flatlines.
+Until, suddenly, it jolts to life. Your old OS is gone. The only thing that meets your eyes is a sickly black screen with a new login page.
+
+
+Some text appears on the screen. 
+
+Press enter to continue. 
+""")
+print("\n\n\nLogging into DRAx Ultim...\n")
+sleep(1)
+print("[|     ] Establishing data connection...\n")
+sleep(1)
+print("[||    ] Copying IP Address...\n")
+sleep(1)
+print("[|||   ] Calculating DRAx Ultim wait times...\n")
+sleep(1)
+print("[||||  ] Sending information to servers...\n")
+sleep(1)
+print("[||||| ] Bypassing safety regulations...\n")
+sleep(1)
+print("[||||||] Finalizing actions...\n")
+sleep(3)
+print("\n\nWelcome to the Dark Web.")
+
+print("DRAx Ultim takes no responsibility for anything that may occur on the dark web.")
+
+# Main loop
+while True:
+
+    # starter commands for beginners. it disappears after 5 commands are ran
+    while commands_ran > 6:
+        print("# Starter commands: dark--mart, commands, user--hack, explore --deeper, logoff, drax-info")  #! FIX THIS!!!
+    choice = input("> ")
+    if choice == "dark--mart":
+        print("\nEntering the dark mart...")
+
+        commands_ran += 1
+
+    
